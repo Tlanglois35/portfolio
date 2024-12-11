af8dec01270b4cb4089f947142722c2953d8e408
Tlanglois35 <thomas.langlois35@gmail.com>
2024-12-11 22:37:06 +0100

    Modification du textP et affichage de la fleche Changes to be committed: 	modified:   src/Header.css 	modified:   src/Project.css 	modified:   src/TexteP.js

--- src/TexteP.js
+++ src/TexteP.js
@@ -1,9 +1,8 @@
 
 function Texte() {
-    return <p>Bonjour ! <br/>Je m'appelle Thomas LANGLOIS, et je suis actuellement en 2ᵉ année de BTS en cycle préparatoire ingénieur à l'Institut Supérieur de l'Électronique et du Numérique (ISEN) de Brest. 
-    Curieux par nature, j'ai toujours été fasciné par l'électronique et ses mécanismes. 
-    Je suis à la recherche d'une alternance de trois ans (2025-2028) dans le domaine de l'électronique, des systèmes embarqués ou de l'automatisme, 
-    en tant qu'apprenti ingénieur. Cette opportunité me permettrait d'accompagner mon parcours à l'ISEN tout en enrichissant mes compétences et mes connaissances pratiques.</p>;
+    return <p>Bonjour ! <br/>Je m'appelle Thomas LANGLOIS, et je suis actuellement en deuxième année de BTS dans le cycle préparatoire ingénieur à l'Institut Supérieur de l'Électronique et du Numérique (ISEN) de Brest.
+Passionné et curieux de nature, j'ai toujours été fasciné par l'électronique et ses mécanismes. Je suis à la recherche d'une alternance de trois ans (2025-2028) dans les domaines de l'électronique, 
+des systèmes embarqués ou de l'automatisme, en tant qu'apprenti ingénieur. Cette opportunité me permettra d'accompagner mon parcours à l'ISEN tout en développant mes compétences et en consolidant mes connaissances pratiques.</p>;
   }
   
   export default Texte;
