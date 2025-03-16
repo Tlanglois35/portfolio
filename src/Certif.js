865694a70ea325700e1e75bbbd14911cd7ad210a
Tlanglois35 <thomas.langlois35@gmail.com>
2025-03-16 17:37:08 +0100

    Ajout du TOEIC et de style + animation
     Changes to be committed:
    	new file:   public/docs/toeic_doc.pdf
    	new file:   public/docs/voltaire_doc.pdf
    	new file:   public/img/TOEIC_logo.png
    	modified:   src/Career.css
    	modified:   src/Certif.css
    	modified:   src/Certif.js
    	modified:   src/Certification.json

--- src/Certif.js
+++ src/Certif.js
@@ -7,6 +7,10 @@ function Certif({ Time }) {
     let textColor = Time === 'day' ? 'black' : 'white';
     let shadColor = Time === 'day' ? '0px 4px 10px rgb(10 11 13 / 20%)' : '0px 4px 10px rgb(10 11 13)';
 
+    const handleOnClick = (item) =>{
+        window.open(item)
+    };
+
     return (
         <Box className="timeline-container">
             <Typography variant="h3" align="center" sx={{ color: textColor, marginBottom: '2rem', lineHeight: 'normal', letterSpacing: 'normal', fontSize: 'calc(1.3rem + 1.6vw)'}}>
@@ -16,11 +20,12 @@ function Certif({ Time }) {
             <Box className="certif">
                 {CertifP.certif.map((item, index) => (
                     <Box key={index} className={`certif-item`}>
-                        <CardMedia sx={{width: '155px', boxShadow: shadColor}}
+                        <CardMedia sx={{width: '155px', boxShadow: shadColor, cursor: 'pointer'}}
                             component="img"
-                            className="timeline-image"
+                            className="timeline-image cert"
                             image={item.src}
                             alt={item.title}
+                            onClick={() => handleOnClick(item.dc)}
                         />
                         <Box className="timeline-content" sx={{ color: textColor }}>
                             <Typography variant="h4" className="timeline-title" style={{lineHeight: 'normal !important'}}>
